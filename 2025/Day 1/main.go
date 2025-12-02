14055a5d7b11dcbda19d1d76f4935f361a765f4e
GoDenisGo <116687893+GoDenisGo@users.noreply.github.com>
2025-12-02 00:26:25 +0000

    AOC 2025 Day 1 Part 1 Solution

---
+++ 2025/Day 1/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// Pakcage main represents Day 1 of AOC, 2025.
+// File input-1.txt contains a list of dial instructions.
+// The dial turns R or L by a certain amount based on the list.
+// The dial only has numbers 0 - 99, going below 0 wraps back to 99 and vice-versa.
+// We need to count the amount of time the dial hits 0.
+//
+// I purposefully avoided the remainder operator (n +or- 100) % 100 because my objective was to have fun here.
+func main() {
+	fmt.Println("AOC 2025 - Day 1!")
+	dial := 50
+	count := 0
+
+	file, err := os.Open("input-1.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	lines := getLines(file)
+	for _, line := range lines {
+		if string(line[0]) == "R" {
+			// add
+			num, err := strconv.Atoi(line[1:])
+			if err != nil {
+				log.Fatal(err)
+			}
+			num = Over(num)
+			// dial = dial + num % 100 // Don't need Over if remainder (%) operator is used
+			dial += num
+		} else {
+			// subtract
+			num, err := strconv.Atoi(line[1:])
+			if err != nil {
+				log.Fatal(err)
+			}
+			num = Over(num)
+			dial -= num
+		}
+
+		// Var "dial" could be Over or Under which isn't legal in this puzzle.
+		dial = Over(dial)
+		dial = Under(dial)
+
+		if dial > 99 {
+			fmt.Println(dial)
+		}
+
+		if dial == 0 {
+			count += 1
+		}
+	}
+
+	fmt.Println(count)
+}
+
+func getLines(f *os.File) []string {
+	var lines []string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+// Over subtracts 100 from n until 0 <= n <= 99.
+func Over(n int) int {
+	if n > 99 {
+		n = Over(n - 100)
+	}
+	return n
+}
+
+// Under adds 100 to n until 0 <= n <= 99.
+func Under(n int) int {
+	if n < 0 {
+		n = Under(n + 100)
+	}
+	return n
+}
