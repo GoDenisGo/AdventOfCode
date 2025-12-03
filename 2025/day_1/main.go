862150e574dd8559fa2f0c42c599728b02de2bdb
GoDenisGo <116687893+GoDenisGo@users.noreply.github.com>
2025-12-03 16:39:05 +0000

    Day 1 Part 2 Solution

--- 2025/day_1/main.go
+++ 2025/day_1/main.go
@@ -8,13 +8,15 @@ import (
 	"strconv"
 )
 
-// Package main represents Day 1 of AOC, 2025.
-// File input-1.txt contains a list of dial instructions.
-// The dial turns R or L by a certain amount based on the list.
-// The dial only has numbers 0 - 99, going below 0 wraps back to 99 and vice-versa.
-// We need to count the amount of time the dial hits 0.
-//
-// If you want to run this code yourself, download the input file for Day 1 Part 1 and rename it to input-1.txt.
+/*
+Package main represents Day 1 of AOC, 2025.
+File input-1.txt contains a list of dial instructions.
+The dial turns R or L by a certain amount based on the list.
+The dial only has numbers 0 - 99, going below 0 wraps back to 99 and vice-versa.
+We need to count the amount of time the dial hits 0.
+
+If you want to run this code yourself, download the input file for Day 1 Part 1 and rename it to input-1.txt.
+*/
 func main() {
 	fmt.Println("AOC 2025 - Day 1!")
 
@@ -42,18 +44,14 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			p = Right(p, r)
+			p, count = Right(p, r, count)
 		} else {
 			// Turn the dial left.
 			l, err := strconv.Atoi(line[1:])
 			if err != nil {
 				log.Fatal(err)
 			}
-			p = Left(p, l)
-		}
-
-		if p.Index == 0 {
-			count++
+			p, count = Left(p, l, count)
 		}
 	}
 
@@ -75,16 +73,22 @@ func getLines(f *os.File) []string {
 	return lines
 }
 
-func Right(p *Node, n int) *Node {
+func Right(p *Node, n, count int) (*Node, int) {
 	for range n {
 		p = p.Right
+		if p.Index == 0 {
+			count++
+		}
 	}
-	return p
+	return p, count
 }
 
-func Left(p *Node, n int) *Node {
+func Left(p *Node, n, count int) (*Node, int) {
 	for range n {
 		p = p.Left
+		if p.Index == 0 {
+			count++
+		}
 	}
-	return p
+	return p, count
 }
