ff8cd7f7013c2f2b6c7c93e1ecac906683c40c5f
GoDenisGo <116687893+GoDenisGo@users.noreply.github.com>
2025-12-03 13:54:07 +0000

    Renamed Linked List file

--- 2025/day_1/CircularDoublyLinkedList.go
+++ 2025/day_1/CircularDoublyLinkedList.go
@@ -3,14 +3,14 @@ package main
 import "log"
 
 /*
-File DoublyCircularLinkedList.go implements a Doubly Circular Linked List.
+File CircularDoublyLinkedList.go implements a Doubly Circular Linked List.
 Made for Advent of Code 2025, this is not a full implementation of the data structures and methods.
 */
 
 /*
-Type DoublyCircularLinkedList is used for simulating a virtual dial.
+Type CircularDoublyLinkedList is used for simulating a virtual dial.
 */
-type DoublyCircularLinkedList struct {
+type CircularDoublyLinkedList struct {
 	Head   *Node
 	Tail   *Node
 	Length int
@@ -24,12 +24,13 @@ type Node struct {
 }
 
 // InsertBeginning places a Node at the start of the Circular Linked List and increments its length.
-func (cl *DoublyCircularLinkedList) InsertBeginning(n *Node) {
+func (cl *CircularDoublyLinkedList) InsertBeginning(n *Node) {
 	if cl.Head == nil {
 		cl.Head = n
 		cl.Tail = cl.Head
 		n.Right = cl.Head
 	} else {
+		cl.Head.Left = n // Previous node will point to new node.
 		n.Right = cl.Head
 		cl.Head = n
 		cl.Tail.Right = cl.Head
@@ -41,7 +42,7 @@ func (cl *DoublyCircularLinkedList) InsertBeginning(n *Node) {
 Find searches for a Node whose Index is equal to i and returns it.
 Crashes with log.Fatal if the Linked List is empty.
 */
-func (cl *DoublyCircularLinkedList) Find(i int) *Node {
+func (cl *CircularDoublyLinkedList) Find(i int) *Node {
 	if cl.Head == nil {
 		log.Fatal("Linked list is empty!")
 		return nil
@@ -49,13 +50,15 @@ func (cl *DoublyCircularLinkedList) Find(i int) *Node {
 
 	if cl.Head.Index == i {
 		return cl.Head
-	} else {
-		h := cl.Head
-		for h.Right != cl.Head {
-			h = h.Right
-			if h.Index == i {
-				return h
-			}
+	}
+
+	h := cl.Head
+	for h.Right != cl.Head {
+		h = h.Right
+		if h.Index == i {
+			return h
 		}
 	}
+
+	return nil
 }
